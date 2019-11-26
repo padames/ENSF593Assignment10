bd0686f03bed0d9863009d84242f8cb429b4649a
padames <pablo.adames@gmail.com>
2019-11-26 15:39:14 -0700

    4 sorting methods

--- src/SortApp.java
+++ src/SortApp.java
@@ -1,8 +1,51 @@
 
 public class SortApp {
 
+	/**
+	 * 
+	 * @param arr
+	 */
+	public static void bubbleSort(int[] arr) {
+		
+	}
+
+	/**
+	 * 
+	 * @param arr
+	 */
+	public static void quickSort(int[] arr) {
+		
+	}
+
+	
+	/**
+	 * 
+	 * @param arr
+	 */
+	public static void mergeSort(int [] arr) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param arr
+	 */
+	public void insertionSort(int[] arr) {
+		for (int i = 1, j; i < arr.length; i++){
+			int temp = arr[i];
+			for (j = i; j > 0 && temp < arr[j-1]; j--)
+			arr[j] = arr[j-1];
+			arr[j] = temp;
+			}
+	}
+
+	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
