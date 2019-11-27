5476d6926b08092f244cad5d63dafe4dd77937e0
padames <pablo.adames@gmail.com>
2019-11-26 17:41:37 -0700

    infrastucture to use command line and run the app

--- src/SortApp.java
+++ src/SortApp.java
@@ -1,5 +1,151 @@
+import org.apache.commons.cli.*;
+
 
 public class SortApp {
+
+	private Options options = new Options();
+	CommandLine cmd;
+	String arrOrder;
+	String algo;
+	String outputFilename;
+	int size;
+	int [] arr;
+	
+	/**
+	 * Entry point for app. Parses the arguments and creates the necessary data to run.
+	 * <p>
+	 * There are four required command line arguments.
+	 * After successfully parsing them it populates the input
+	 * variables and leaves the app in a state suitable to start a run.
+	 * @param args
+	 */
+	public SortApp(String[] args) {
+		var arrOrderOpt = addRequiredOption("o", "order", "order for the input array: ascending, descending, random");
+		options.addOption(arrOrderOpt);
+		
+		var arrSizeOpt = addRequiredOption("s", "size", "size of the array to sort");
+		options.addOption(arrSizeOpt);
+		
+		var algoChoiceOpt = addRequiredOption("a", "algorithm", "sorting algorithm: bubble, insertion, merge, quick");
+		options.addOption(algoChoiceOpt);
+		
+		var outFileNameOpt = addRequiredOption("f", "filename", "output file name for results");
+		options.addOption(outFileNameOpt);
+		
+		parseArguments(args);
+		
+		populateValues();
+	}
+	
+	/**
+	 * having parsed the command line arguments, populate the input variables
+	 * with the command line options
+	 */
+	private void populateValues() {
+		
+		arrOrder = cmd.getOptionValue("order");
+		
+		String sizeS = cmd.getOptionValue("size");
+		size = Integer.valueOf(sizeS);
+		
+		algo = cmd.getOptionValue("algorithm");
+		
+		outputFilename = cmd.getOptionValue("filename");
+		
+	}
+
+	/**
+	 * populate the command line member variable with the options from parsing the arguments.
+	 * <p>
+	 * If there is a problem in the command line arguments it will 
+	 * print out the help and exit. 
+	 * @param args the command line arguments given to the program
+	 */
+	private void parseArguments(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            cmd = parser.parse(options, args);
+        } 
+        catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("Sort-App", options);
+
+            System.exit(1);
+        }
+	}
+
+	private void createArray() {
+		switch(arrOrder) {
+		case ("ascending"):
+			
+			break;
+		case ("descending"):
+			
+			break;
+		case ("random"):
+			
+			break;
+		}
+	}
+	
+	private Option addRequiredOption(String shortArg, String longArg,
+			String description) {
+		var theOption = new Option(shortArg, longArg, true, description);
+		theOption.setRequired(true);
+		return theOption;
+	}
+	
+	/**
+	 * Having parsed the command line argument it executes the 
+	 * options entered by the user.
+	 * Finally it saves result to file.
+	 */
+	private void run() {
+	
+		if (cmd == null) {
+			System.err.println("Something went wrong, no commanline arguments were parsed.");
+			System.exit(1);
+		}
+
+		createArray();
+		
+		sort();
+
+		saveResultsToFile();
+	}
+	
+	/**
+	 * executes sorting algorithm indicated by user
+	 */
+	private void sort() {
+		
+		switch (algo) {
+		case ("bubble"):
+			SortApp.bubbleSort(arr);
+			break;
+		case ("insertion"):
+			SortApp.insertionSort(arr);
+			break;
+		case ("merge"):
+			SortApp.mergeSort(arr, arr[0], arr[size-1]);
+			break;
+		
+		case("quick"):
+			SortApp.quickSort(arr);
+			break;
+		}		
+	}
+
+	/**
+	 * precondition: filename exists and array has been sorted
+	 * saves sorted array to text file one value per line
+	 */
+	private void saveResultsToFile() {
+		// TODO grab the sorted array and save it to file
+		
+	}
 
 	/**
 	 * 
@@ -91,7 +237,7 @@ public class SortApp {
 	 * O(n) if array is nearly ordered. 
 	 * @param arr an array of integers
 	 */
-	public void insertionSort(int[] arr) {
+	public static void insertionSort(int[] arr) {
 		for (int i = 1, j; i < arr.length; i++){
 			int temp = arr[i];
 			for (j = i; j > 0 && temp < arr[j-1]; j--) {
@@ -101,15 +247,26 @@ public class SortApp {
 		}
 	}
 
+
 	
 	/**
 	 * Expected execution from command line:
 	 * java SortApp order size algorithm outputfile
-	 * @param args
+	 * <p>
+	 * <b>Note: </b> Using the extension commons-cli to
+	 * parse the command line options 
+	 * (see: http://commons.apache.org/proper/commons-cli/introduction.html)
+	 * It is installed locally in the folder /lib/ext as a Jar file and 
+	 * has been added to the class path pf the Eclipse project
+	 * Idea from https://stackoverflow.com/a/367714/1585486
+	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
 		
-
+		var app = new SortApp(args);
+		
+		app.run();
+		
 	}
 
 }
